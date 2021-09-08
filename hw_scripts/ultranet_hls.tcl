72f2d9d92c54790ddd217faa14069bbf88befed2
microideax <yaochen_nk@hotmail.com>
2021-09-08 23:00:24 +0800

    bitstreams for test

--- hw_scripts/ultranet_hls.tcl
+++ hw_scripts/ultranet_hls.tcl
@@ -3,13 +3,13 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project HLS
+open_project ultra_core
 set_top ultra_net
-add_files ./src/ultranet.cpp -cflags "-std=c++11" -csimflags "-std=c++11"
+add_files ../src/ultranet.cpp -cflags "-std=c++11" -csimflags "-std=c++11"
 
-open_solution "solution1"
+open_solution "ultracore_125"
 set_part {xczu3eg-sbva484-1-e}
-create_clock -period 4 -name default
+create_clock -period 8 -name default
 #source "./HLS_SINGLE/solution1/directives.tcl"
 #csim_design
 csynth_design
